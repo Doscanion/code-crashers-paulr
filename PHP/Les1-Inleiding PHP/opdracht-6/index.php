3827dcfa5177dfdfbd5a6b6ce30406ee000bbdfe
Doscanion <paulrijsdijk30@msn.com>
2024-02-06 23:52:31 +0100

    CSS en PHP

---
+++ PHP/Les1-Inleiding PHP/opdracht-6/index.php
@@ -0,0 +1,17 @@
+<?php
+    $string = 'The club isn\'t the best place to find a lover, so the bar is where I go. Me and my friends at the table doing shots, drinking fast and then we talk slow.'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?= strpos($string, 'o') ?></p>
+    <p><?= strrpos($string, 't') ?></p>
+    <p><?= str_shuffle($string) ?></p>
+</body>
+</html>
